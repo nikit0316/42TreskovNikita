14e9842fead1b55abe8af6f3c40f5d9576af17b3
Nikita Treskov <treskov-1@mail.ru>
2020-03-12 17:26:07 +0300

    Task 3 complete

---
+++ 3/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form>
+        <textarea name="textinput" placeholder="Enter your text here"></textarea><br>
+        <button>go</button>
+    </form>
+    <?php
+    $initial_text = $_REQUEST['textinput'] ?? '';
+    $strok_arr = explode(PHP_EOL, $initial_text);
+    $count = count($strok_arr);
+    for ($i = 0; $i < $count; $i++) {
+        $stroka = randomiser($strok_arr[$i]);
+        while (in_array($stroka, $strok_arr)) {
+            $stroka = randomiser($strok_arr[$i]);
+        }
+        array_push($strok_arr, $stroka);
+    }
+    print_r($strok_arr);
+
+    $keys = sorting($strok_arr);
+    for ($i = 0; $i < count($strok_arr); $i++) {
+        $stroka = $strok_arr[$keys[$i]];
+        echo "<br> $stroka";
+    }
+
+    function randomiser(string $stroka)
+    {
+        $words_arr = explode(" ", $stroka);
+        shuffle($words_arr);
+        $randomed_words = "";
+        for ($i = 0; $i < count($words_arr); $i++) {
+            if ($randomed_words == "") {
+                $randomed_words = $words_arr[$i];
+            } else {
+                $randomed_words = $randomed_words . " " . $words_arr[$i];
+            }
+        }
+        return $randomed_words;
+    }
+
+    function sorting(array $stroki)
+    {
+        $words_arr = array();
+        for ($i = 0; $i < count($stroki); $i++) {
+            $words_arr[$i] = explode(" ", $stroki[$i])[1];
+        }
+        natcasesort($words_arr);
+        print_r($words_arr);
+        $keys = array_keys($words_arr);
+        return $keys;
+    }
+    ?>
+</body>
+
+</html>
